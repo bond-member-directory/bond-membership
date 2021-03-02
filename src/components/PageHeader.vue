54e2753d47be0e4c42e841f4e3e12b4b88fde9f9
David Kane <david@dkane.net>
2021-03-02 21:29:43 +0000

    add text to page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -2,7 +2,22 @@
   <header class="w-100 bg-white mb3">
     <div class="w-100 f4">
         <img alt="Bond logo" src="../assets/logo.png" class="w5-l w4">
-        <h1 class="bond-mid-grey ml5-l header-text fr-l normal f1-l f2">Membership directory</h1>
+        <div class="ml5-l fr-l tr pv3">
+          <h1 class="bond-mid-grey header-text normal f1-l f2 mt0 mb3">Network Map</h1>
+          <p class="measure f5 ma0 pa0 lh-copy">
+            Browse the international development organisations that are members of 
+            <a href="https://www.bond.org.uk/about-us" class="bond-red link underline bond-link b">the Bond network</a>, 
+            and see which countries they work in and which Sustainable Development Goals they focus on.
+          </p>
+          <!--<p class="measure f5 ma0 pa0 lh-copy">
+            You can view the results of your search as a listing or a map. You can also search by an 
+            organisation’s name. 
+          </p>-->
+          <p class="measure f5 ma0 pa0 lh-copy">
+            If you have any feedback on this directory, please 
+            <a href="https://www.bond.org.uk/contact-us" class="bond-red link underline bond-link b">get in touch</a>.
+          </p>
+        </div>
     </div>
   </header>
 </template>
